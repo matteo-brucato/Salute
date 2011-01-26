1fee8802023998a7f3368b7e229e1e6fb1b31bf3
Nada <nadahashem@gmail.com>
2011-01-25 18:08:34 -0800

    More work on login controller

--- system/application/controllers/homepage.php
+++ system/application/controllers/homepage.php
@@ -24,7 +24,25 @@ class Homepage extends Controller {
 		// 	else -> load error view
 		$email = $this->input->post('email');
 		$password = $this->input->post('password');
-		echo "$email $password";
+//		echo "$email $password";
+
+		$this->load->model('login_model');
+		$results = $this->login_model->try_login(array("email" => $email,"password" => $password); // <-- this will be an array
+
+		// if login fails, only change side panel w/ login failed message
+		if ($results === NULL) {
+			$this->ajax->view(array(
+			'',
+			$this->load->view('login_failed', '', TRUE)
+		}
+
+		// login successful, store info for session id, go to user profile
+		else{
+
+		}
+
+		));
+
 	}
 
 	function logout()
