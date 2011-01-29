d88a64dd9a4757edc418f907c38f349579678a6a
Nada <nadahashem@gmail.com>
2011-01-29 00:50:33 -0800

    Appointments Controller: waiting on forms and views, reschedule and request are still incomplete. Bills Controller: all implemented, waiting on views/models

--- system/application/controllers/appointments.php
+++ system/application/controllers/appointments.php
@@ -14,7 +14,9 @@ class Appointments extends Controller {
 		$this->ajax->redirect('/appointments/upcoming');
 	}  	
 
-	/* List all Appointments */
+	/* List all Appointments 
+		@todo: view to list appointments: Date Time Descrip Doctor Name Actions(Reschedule,Cancel)	
+	*/
 	function all() {
 		$this->auth->check_logged_in();
 
@@ -23,13 +25,15 @@ class Appointments extends Controller {
 									'type' => $this->auth->get_type()
 								)); 
 		$this->ajax->view(array(
-					'',
-					$this->load->view('mainpane/table_result', $results , TRUE)
+					$this->load->view('mainpane/____', $results , TRUE),
+					''
 				));
 	}
 
 
-	/* List Upcoming Appointments */
+	/* List Upcoming Appointments
+		@todo: view to list appointments: Date Time Descrip Doctor Name Actions(Reschedule,Cancel)	
+	*/
 	function upcoming(){
 		$this->auth->check_logged_in();
 
@@ -37,10 +41,7 @@ class Appointments extends Controller {
 									'account_id' => $this->auth->get_account_id(),
 									'type' => $this->auth->get_type()
 								)); 
-		$this->ajax->view(array(
-					'',
-					$this->load->view('mainpane/table_result', $results , TRUE)
-				));
+		$this->ajax->view(array($this->load->view('mainpane/_____', $results , TRUE),''));
 	}
 
 	/* List Past Appointments */
@@ -51,42 +52,52 @@ class Appointments extends Controller {
 									'account_id' => $this->auth->get_account_id(),
 									'type' => $this->auth->get_type()
 								)); 
-		$this->ajax->view(array(
-					'',
-					$this->load->view('mainpane/table_result', $results , TRUE)
-				));
-
+		$this->ajax->view(array($this->load->view('mainpane/table_result', $results , TRUE),''));
 	}
 
 	/*Cancel an existing appointment */	
 	function cancel($apt_id) {
 		$this->auth->check_logged_in();
-		$results = $this->appointments_model->cancel(array('appointment_id' => $apt_id)); 
-		// TODO: Refresh page
+		$this->load->model('appointments_model');
+		if($this->appointments_model->is_myappointment(array($this->auth->get_account_id(),$apt_id))){
+			/* @to do: pop up-- are you sure you want to cancel appointment?*/
+			$results = $this->appointments_model->cancel(array($apt_id)); 
+			$this->ajax->redirect('/appointments/upcoming');
+		}
+		else{
+			show_error('This is not your appointment. Permission Denied.', 500);
+			return;
+		}
+		
 	}
 	
 	/*Reschedule an existing appointment (date/time) */
-	// TODO: need reschedule appt form / view
+	// @todo: need reschedule appt form / view
+	// Later: we should have a request reschedule and accept reschedule 
 	function reschedule($apt_id){
 		$this->auth->check_logged_in();
-		$this->ajax->view(array(
-					'',
-					$this->load->view('mainpane/reschedule_appointments', $results , TRUE)
-				));
-		$new_time = $this->input->post('appointment_time');
-		$results = $this->appointments_model->reschedule(array('appointment_id' => $apt_id, 'new_time' => $new_time )); 
+		$this->load->model('appointments_model');
+		if($this->appointments_model->is_myappointment(array($this->auth->get_account_id(),$apt_id))){
+			$result = $this->appointments_model->get_appointment(array($apt_id));
+			$this->ajax->view(array($this->load->view('mainpane/________',$result, TRUE),''));
+			$new_time = $this->input->post('appointment_time');
+			$results = $this->appointments_model->reschedule(array('appointment_id' => $apt_id, 'date_time' => $new_time )); 
+		}
+		else{
+			show_error('This is not your appointment. Permission Denied.', 500);
+			return;
+		}
 	}
 
 	/* request an appointment */      	   
 	//TODO: view for appt request ; 
 	// expect an array with doctorid, reason for appointment, time
+	/* Fancy: send confirmation */
+	// do the same thing as a connection request...wont implement this till that one is figured out 
 	function request(){
 		$this->auth->check_logged_in();
 
-		$this->ajax->view(array(
-					'',
-					$this->load->view('mainpane/request_appointments', '' , TRUE)
-				));
+		$this->ajax->view(array($this->load->view('mainpane/__________', '' , TRUE), ''));
 		$hcp_id = $this->input->post('hcp_id');
 		$desc = $this->input->post('description');
 		$time = $this->input->post('time');
@@ -95,12 +106,6 @@ class Appointments extends Controller {
 									'desc' => $desc ,
 									'time' => $time 
 								)); 
-		/* Fancy: send confirmation 
-		$this->ajax->view(array(
-					'',
-					$this->load->view('mainpane/confirmation', '' , TRUE)
-				));
-		*/
 	}
 }
 ?>
--- system/application/controllers/bills.php
+++ system/application/controllers/bills.php
@@ -10,27 +10,63 @@ class Bills extends Controller {
 	/* Default: call all function */
 	function index(){
 		$this->auth->check_logged_in();
+		$this->ajax->redirect('/bills/all');
 	}
   	
 	/* List all bills  */      	   
+	// @todo: patient main panel view : to list bills : ( Date, Title, Doctor Name, Amount, Status(unpaid/paid/pending), Actions (Pay Now, View Receipt)  
+	// @todo: doctor main panel view : to list bills : ( Date, Title, Doctor Name, Amount, Status(unpaid/paid/pending), Actions (Delete,View Receipt)  
 	function all()	{
 		$this->auth->check_logged_in();
-		// if Doctor --> Additional Option: list all bills of a specific patient id
-		// 	e.g. Doctor wants to see the all bills a specific patient has with him/her
+		$this->load->model('bills_model');
+		if($this->auth->get_type() === 'patient'){
+			$results = $this->bills_model->view_all(array($this->auth->get_account_id(),$this->auth->get_type()));
+			$this->ajax->view(array($this->load->view('mainpane/____________', $results, TRUE),''));
+		} 
+		else if($this->auth->get_type() === 'doctor'){
+			$results = $this->bills_model->view_all(array($this->auth->get_account_id(),$this->auth->get_type()));
+			$this->ajax->view(array($this->load->view('mainpane/____________', $results, TRUE),''));
+		} 
+		else{
+			show_error('Error: unable to list your bills.', 500);
+			return;		
+		}
 	}
 
 	/* List Current Bills */
 	function current(){
 		$this->auth->check_logged_in();
-		// if Doctor --> Additional Option: list all current bills of a specific patient id
-		// 	e.g. Doctor wants to see the current bills a specific patient owes him/her
+		$this->load->model('bills_model');
+		if($this->auth->get_type() === 'patient'){
+			$results = $this->bills_model->view_current(array($this->auth->get_account_id(),$this->auth->get_type()));
+			$this->ajax->view(array($this->load->view('mainpane/____________', $results, TRUE),''));
+		} 
+		else if($this->auth->get_type() === 'doctor'){
+			$results = $this->bills_model->view_current(array($this->auth->get_account_id(),$this->auth->get_type()));
+			$this->ajax->view(array($this->load->view('mainpane/____________', $results, TRUE),''));
+		} 
+		else{
+			show_error('Error: unable to list your bills.', 500);
+			return;		
+		}
 	}
 
 	/* Lists past bills */	
 	function past() {
 		$this->auth->check_logged_in();
-		// if Doctor --> Additional Option: list all past bills of a specific patient id
-		// 	e.g. Doctor wants to see all the bills a specific patient paid him/her
+		$this->load->model('bills_model');
+		if($this->auth->get_type() === 'patient'){
+			$results = $this->bills_model->view_past(array($this->auth->get_account_id(),$this->auth->get_type()));
+			$this->ajax->view(array($this->load->view('mainpane/____________', $results, TRUE),''));
+		} 
+		else if($this->auth->get_type() === 'doctor'){
+			$results = $this->bills_model->view_past(array($this->auth->get_account_id(),$this->auth->get_type()));
+			$this->ajax->view(array($this->load->view('mainpane/____________', $results, TRUE),''));
+		} 
+		else{
+			show_error('Error: unable to list your bills.', 500);
+			return;		
+		}
 	}
 
 	// load form , charge patient an amount for an procedure/appointment/test, upload itemized receipt	
@@ -38,11 +74,34 @@ class Bills extends Controller {
 	// Only available for doctors
 	function issue_new_bill() {
 		$this->auth->check_logged_in();
+		$this->load->model('bills_model');
+		if($this->auth->get_type() === 'doctor'){
+			$patient_id = $this->input->post('patient_id');
+			$amount = $this->input->post('amount');
+			$description = $this->input->post('description');
+			$due_date = $this->input->post('due_date');
+			$this->bills_model->issue_bill(array($patient_id,$this->auth->get_account_id(),$amount,$description,$due_date));
+		}
+		else{
+			show_error('Error: only doctors can issue bills.', 500);
+			return;		
+		}
+		$this->ajax->redirect('/bills');
+
 	}
 
 	// only available to patient: pay a bill
-	function pay() {
+	function pay($bill_id) {
 		$this->auth->check_logged_in();	
+		$this->load->model('bills_model');
+		if($this->auth->get_type() === 'patient' && $this->bills_model->is_mybill(array($this->auth->get_account_id(),$bill_id)) ){
+			$this->bills_model->pay_bill(array($this->auth->get_account_id(),$amount));
+		}
+		else{
+			show_error('Error: only patients can pay bills.', 500);
+			return;		
+		}
+		$this->ajax->redirect('/bills');
 	}
 
 }
