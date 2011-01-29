d936c052a83692f62352806dea449c09c7b34274
Matteo Brucato <brucato@cs.unibo.it>
2011-01-28 20:29:25 -0800

    connections/accept

--- system/application/controllers/connections.php
+++ system/application/controllers/connections.php
@@ -107,36 +107,44 @@ class Connections extends Controller {
 //		$this->email->to($results['email']);
 		$this->email->to('nadahashem@gmail.com');
 		$this->email->subject('New Connection Request');
-
+		
+		
 		// @todo: Fix the body -- need to put a link to 'accept' connection, connections/accept/requester_id
 		$this->email->message(
 			'You have a new connection request from '.
 			$this->auth->get_first_name().' '.$this->auth->get_last_name().
-			'. Click <a href="connections/accept/'.$this->auth->get_account_id().'/'.$id.'">here</a> to accept.');
+			'. Click <a href="https://'.$_SERVER['SERVER_NAME'].'/connections/accept/'.$this->auth->get_account_id().'/'.$id.'">here</a> to accept.');
 
 		// @todo: Pop-up : are you sure you want to send?
 		$this->email->send();
 	}
 
 	// Accept request ; Note: only Doctors do this 
-	function accept($requestor_id, $my_id) // this needs to take in an account id as an argument. 
+	function accept($requester_id, $my_id) // this needs to take in an account id as an argument. 
 	{
 		$this->auth->check_logged_in();
 		
-		$this->load->model('connections_model');
+		if ($this->auth->get_account_id() != $my_id) {
+			show_error('You are not the receiver for this request');
+			return;
+		}
 		
-		if ( $this->auth->get_type() === 'patient' ){
-			$this->connections_model->accept_patient_doctor(array('patient_id' => $id, 'hcp_id' => $this->auth->get_account_id() ));
+		$this->load->model('connections_model');
+		$this->load->model('hcp_model');
+		$this->load->model('patient_model');
+		
+		if ($this->patient_model->is_patient(array($requester_id))) {
+			$this->connections_model->accept_patient_doctor(array('patient_id' => $requester_id, 'hcp_id' => $this->auth->get_account_id() ));
 		}
 
-		else if ( $this->auth->get_type() === 'doctor' ){
-			$this->connections_model->accept_doctor_doctor(array('hcp_id' => $id, 'this_hcp_id' => $this->auth->get_account_id() ));
+		else if ($this->hcp_model->is_doctor(array($requester_id))) {
+			$this->connections_model->accept_doctor_doctor(array('hcp_id' => $requester_id, 'this_hcp_id' => $this->auth->get_account_id() ));
 		}
 
 		else {
-			show_error('Unknown Error.', 500);
+			show_error('The requester id does not match any id in the database', 500);
 			return;
-		}		
+		}
 	}
 
 	// destroy connection (un-friend someone)
