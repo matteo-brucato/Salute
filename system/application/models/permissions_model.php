14f761cf67f456bb63566a5345cab55bf004da01
mj16amn <mj16amn@yahoo.com>
2011-01-28 18:48:11 -0800

    added the permissions_models.php table

---
+++ system/application/models/permissions_model.php
@@ -0,0 +1,42 @@
+<?php
+class Permissions_model extends Model {
+	
+	function __construct() {
+		parent::Model();
+		$this->load->database();
+	}
+	
+	/**
+	 * Allows an account_id permission to see medical record
+	 * 
+	 * @param $inputs
+	 *   Is of the form: array(medical_rec_id, account_id)
+	 * @return
+	 *   Inserts a row in the Permissions table
+	 * */
+	function allow_permission($inputs){
+	
+		$data = array( 'medical_rec_id' => $inputs[0], 'account_id' => $inputs[1]);
+		$this->db->insert( 'Permissions', $data);
+	
+	}
+	
+	/**
+	 * Removes the ability for an account_id to view a medical record
+	 * 
+	 * @param $inputs
+	 *   Is of the form: array(medical_rec_id, account_id)
+	 * @return
+	 *   Deletes a row in the Permissions table
+	 * */
+	function delete_permission($inputs){
+	
+		$this->db->delete('Permissions', array( 'medical_rec_id' => $inputs[0], 'account_id' => $inputs[1]);
+	
+	}
+
+
+
+
+
+?>
