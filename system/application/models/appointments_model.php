3cd80202d3518b17da21881820ba5b73c86a6b11
MR <musa@musa-laptop.(none)>
2011-01-30 17:01:00 -0800

    added error checking into appointments_model.php.  added error code -5 as appointment_id does not exist

--- system/application/models/appointments_model.php
+++ system/application/models/appointments_model.php
@@ -12,17 +12,36 @@ class Appointments_model extends Model {
 	 * @param $inputs
 	 *   Is of the form: array(patient_id, appointment_id)
 	 * @return
-	 *   Returns TRUE if it is, FALSE otherwise
+	 *   -1 in case of error in a query
+	 *   -5 appointment id does not exist
+	 *    TRUE if it is
+	 * 	  FALSE otherwise
 	 * */
 	 function is_myappointment($inputs){
-	 
-	 	$sql = "Select *
-	 		FROM appointments A
-	 		WHERE A.patient_id = ? AND A.appointment_id = ?";
+		 
+		//test to see if the appointment_id exists
+		$sql = "SELECT *
+				FROM appointments A
+				WHERE A.appointment_id = ?"
+		$query = $this->db->query($sql, array($inputs[1]));
+		
+		if ($this->db->trans_status() === FALSE)
+			return -1;
+			
+		if ($query->num_rows() < 1)
+			return -5;
+
+	 	$sql = "SELECT *
+				FROM appointments A
+				WHERE A.patient_id = ? AND A.appointment_id = ?";
 	 	$query = $this->db->query($sql, $inputs);
-	 	$result = $query->result_array();
-	 	if ( count($result) > 0 )
-	 		return TRUE;
+	 	
+	 	if ($this->db->trans_status() === FALSE)
+			return -1;
+			
+	 	if ($query->num_rows() > 0)
+			return TRUE;
+
 	 	return FALSE;
 	 }
 
@@ -33,24 +52,48 @@ class Appointments_model extends Model {
 	 * @param $inputs
 	 *   Is of the form: array(appointment_id)
 	 * @return
-	 *   Array with all the whole tuple from Appointments table, plus patient first_name and last_name, doctor first_name and last_name 
+	 *   -1 in case of error in a query
+	 *   -5 appointment id does not exist
+	 *    Array with all the whole tuple from Appointments table, plus patient first_name and last_name, doctor first_name and last_name 
 	 * */
 	 function get_appointment($inputs){
+		 
+		//test to see if the appointment_id exists
+		$sql = "SELECT *
+				FROM appointments A
+				WHERE A.appointment_id = ?"
+		$query = $this->db->query($sql, $inputs);
+		
+		if ($this->db->trans_status() === FALSE)
+			return -1;
+			
+		if ($query->num_rows() < 1)
+			return -5;
 	 
 	 	$sql = "SELECT A.*, P.first_name AS pat_first_name, P.last_name AS pat_last_name, 
 	 			    H.first_name AS hcp_first_name, H.last_name AS hcp_last_name 
-	 		FROM appointments A, hcp_account H, patient_account P 
-	 		WHERE A.appointment_id = ? AND A.patient_id = P.account_id AND A.hcp_id = H.account_id";
+				FROM appointments A, hcp_account H, patient_account P 
+				WHERE A.appointment_id = ? AND A.patient_id = P.account_id AND A.hcp_id = H.account_id";
 	 	$query = $this->db->query($sql, $inputs);
-	 	$result = $query->result_array();
-	 	return $result;
+	 	
+	 	if ($this->db->trans_status() === FALSE)
+			return -1;
+		
+	 	return $query->result_array();
 	 }
 	 
 	 
-	//patient requests an appointment with a doctor
-	//I assume $inputs will be of the form (patient_id, doctor_id, descryption, date_time (YY-MM-DD HH-MM-SS))
-	//inserts an entry into the Appointments table
-	//NOTE: AS OF RIGHT NOW IT DOES NOT HANDLE ERROR CHECKING TO SEE IF THE date_time SPECIFIED CONFLICTS WITH ANOTHER APPOINTMENT
+	/**
+	 * Patient requests an appointment with a doctor
+	 * 
+	 * @param $inputs
+	 *   Is of the form: array(patient_id, doctor_id, descryption, date_time (YY-MM-DD HH-MM-SS))
+	 * @return
+	 *   -1 in case of error in a query
+	 *    0 if everything goes fine and an entry into the appointments table is made
+	 * @note
+	 * 	  AS OF RIGHT NOW IT DOES NOT HANDLE ERROR CHECKING TO SEE IF THE date_time SPECIFIED CONFLICTS WITH ANOTHER APPOINTMENT
+	 * */
 	function request($inputs){
 	
 		/*FOR LATER, WHEN WE HAVE TO MANAGE TIMES, NOT COMPLETE
@@ -70,22 +113,38 @@ class Appointments_model extends Model {
 			VALUES (?, ?, ?, ?)";
 		$query = $this->db->query($sql, $inputs);
 		
+		if ($this->db->trans_status() === FALSE)
+			return -1;
+			
+		return 0;
+		
 	}
 	
-	//view all appointments a patient has ever had OR all appointments a doctor has ever issued (approved as well as not approved)
-	//I assume $inputs will be of the form (account_id, type of account(doctor or patient))
-	//returns array with all appointments OR NULL if there are no appointments
+
+	/**
+	 * 	View all appointments a patient has ever had OR all appointments a doctor has ever issued (approved as well as not approved)
+	 * 
+	 * @param $inputs
+	 *   Is of the form: array(account_id, type of account(doctor or patient))
+	 * @return
+	 *   -1 in case of error in a query
+	 *    Array with all appointments
+	 * 	  NULL if there are no appointments
+	 * */
 	function view_all($inputs){
 	
 		//lists all appointments a patient has ever had
 		if( $inputs[1] == 'patient'){
 			$sql = "Select A.appointment_id, H2.first_name, H2.last_name, A.amount, A.descryption, A.date_time, A.cleared
-				FROM appointments A, hcp_account H, hcp_account H2
-				WHERE A.hcp_id = H.account_id AND A.patient_id = ? AND A.hcp_id == H2.account_id";
+					FROM appointments A, hcp_account H, hcp_account H2
+					WHERE A.hcp_id = H.account_id AND A.patient_id = ? AND A.hcp_id == H2.account_id";
 			$query = $this->db->query($sql, array($inputs[0]));
-			$result = $query->result_array();
-			if( count($result) > 0 )
-				return $result;
+			
+			if ($this->db->trans_status() === FALSE)
+				return -1;
+			
+			if ($query->num_rows() > 0)
+				return $query->result_array();
 
 			return NULL;	
 		}
@@ -95,118 +154,223 @@ class Appointments_model extends Model {
 			FROM appointments A, patient_account P, patient_account P2
 			WHERE A.patient_id = P.account_id AND A.hcp_id = ? AND A.patient_id == P2.account_id";
 		$query = $this->db->query($sql, array($inputs[0]));
-		$result = $query->result_array();
-		if( count($result) > 0 )
-			return $result;
-
+		
+		if ($this->db->trans_status() === FALSE)
+				return -1;
+			
+		if ($query->num_rows() > 0)
+			return $query->result_array();
+			
 		return NULL;			
 	}
 	
-	//view all upcoming appointments a patient has OR all upcoming appointments a doctor has (approved as well as not approved)
-	//I assume $inputs will be of the form (account_id, type of account(doctor or patient))
-	//returns array with all upcoming appointments OR NULL if there are no upcoming appointments
-	//NOTE: I DETERMINE WHAT IS UPCOMING IF THE APPOINTMENT date_time ATTRIBURE >= NOW() (NOW RETURNTS CURRENT DATE AND TIME YY-MM-DD HH:MM:SS)
+	
+	/**
+	 * View all upcoming appointments a patient has OR all upcoming appointments a doctor has (approved as well as not approved)
+	 * 
+	 * @param $inputs
+	 *   Is of the form: array(account_id, type of account(doctor or patient))
+	 * @return
+	 *   -1 in case of error in a query
+	 *    Array with all upcoming appointments
+	 *    NULL if there are no upcoming appointments
+	 * @note
+	 * 	  I DETERMINE WHAT IS UPCOMING IF THE APPOINTMENT date_time ATTRIBURE >= NOW() (NOW RETURNTS CURRENT DATE AND TIME YY-MM-DD HH:MM:SS)	
+	 * */
 	function view_upcoming($inputs){
 		
 		//lists all upcoming appointments a patient has
 		if( $inputs[1] == 'patient'){
 			$sql = "Select A.appointment_id, H2.first_name, H2.last_name, A.amount, A.descryption, A.date_time, A.cleared
-				FROM appointments A, hcp_account H, hcp_account H2
-				WHERE A.hcp_id = H.account_id AND A.patient_id = ? AND A.hcp_id == H2.account_id AND A.date_time >= NOW()";
+					FROM appointments A, hcp_account H, hcp_account H2
+					WHERE A.hcp_id = H.account_id AND A.patient_id = ? AND A.hcp_id == H2.account_id AND A.date_time >= NOW()";
 			$query = $this->db->query($sql, $inputs[0]);
-			$result = $query->result_array();
-			if( count($result) > 0 )
-				return $result;
+			
+			if ($this->db->trans_status() === FALSE)
+				return -1;
+			
+			if ($query->num_rows() > 0)
+				return $query->result_array();
 
 			return NULL;	
 		}
 
 		//lists all upcoming appointments a doctor has
 		$sql = "Select A.appointment_id, P2.first_name, P2.last_name, A.amount, A.descryption, A.date_time, A.cleared
-			FROM appointments A, patient_account P, patient_account P2
-			WHERE A.patient_id = P.account_id AND A.hcp_id = ? AND A.patient_id == P2.account_id and A.date_time >= NOW()";
+				FROM appointments A, patient_account P, patient_account P2
+				WHERE A.patient_id = P.account_id AND A.hcp_id = ? AND A.patient_id == P2.account_id and A.date_time >= NOW()";
 		$query = $this->db->query($sql, $inputs[0]);
-		$result = $query->result_array();
-		if( count($result) > 0 )
-			return $result;
+		
+		if ($this->db->trans_status() === FALSE)
+				return -1;
+			
+		if ($query->num_rows() > 0)
+			return $query->result_array();
 
 		return NULL;			
 	}
 	
-	//view all past appointments a patient has had OR all past appointments a doctor has had (approved as well as not approved)
-	//I assume $inputs will be of the form (account_id, type of account(doctor or patient))
-	//returns array with all past appointments OR NULL if there are no past appointments
-	//NOTE: I DETERMINE WHAT HAS PAST IF THE APPOINTMENT date_time ATTRIBURE < NOW() (NOW RETURNTS CURRENT DATE AND TIME YY-MM-DD HH:MM:SS)
+
+	/**
+	 * View all past appointments a patient has had OR all past appointments a doctor has had (approved as well as not approved)
+	 * 
+	 * @param $inputs
+	 *   Is of the form: array(account_id, type of account(doctor or patient))
+	 * @return
+	 *   -1 in case of error in a query
+	 *    Array with all past appointments
+	 *    NULL if there are no past appointments
+	 * @note
+	 * 	  I DETERMINE WHAT IS PAST IF THE APPOINTMENT date_time ATTRIBURE < NOW() (NOW RETURNTS CURRENT DATE AND TIME YY-MM-DD HH:MM:SS)	
+	 * */
 	function view_past($inputs){
 			
 		//lists all past appointments a patient has had 
 		if( $inputs[1] == 'patient'){
 			$sql = "Select A.appointment_id, H2.first_name, H2.last_name, A.amount, A.descryption, A.date_time, A.cleared
-				FROM appointments A, hcp_account H, hcp_account H2
-				WHERE A.hcp_id = H.account_id AND A.patient_id = ? AND A.hcp_id == H2.account_id AND A.date_time < NOW()";
+					FROM appointments A, hcp_account H, hcp_account H2
+					WHERE A.hcp_id = H.account_id AND A.patient_id = ? AND A.hcp_id == H2.account_id AND A.date_time < NOW()";
 			$query = $this->db->query($sql, $inputs[0]);
-			$result = $query->result_array();
-			if( count($result) > 0 )
-				return $result;
+			
+			if ($this->db->trans_status() === FALSE)
+				return -1;
+			
+			if ($query->num_rows() > 0)
+				return $query->result_array();
 
 			return NULL;	
 		}
 
 		//lists all past appointments a doctor has had
 		$sql = "Select A.appointment_id, P2.first_name, P2.last_name, A.amount, A.descryption, A.date_time, A.cleared
-			FROM appointments A, patient_account P, patient_account P2
-			WHERE A.patient_id = P.account_id AND A.hcp_id = ? AND A.patient_id == P2.account_id and A.date_time < NOW()";
+				FROM appointments A, patient_account P, patient_account P2
+				WHERE A.patient_id = P.account_id AND A.hcp_id = ? AND A.patient_id == P2.account_id and A.date_time < NOW()";
 		$query = $this->db->query($sql, $inputs[0]);
-		$result = $query->result_array();
-		if( count($result) > 0 )
-			return $result;
-
+		
+		if ($this->db->trans_status() === FALSE)
+				return -1;
+			
+		if ($query->num_rows() > 0)
+			return $query->result_array();
+		
 		return NULL;
 	}
 	
-	//doctor approves an appointment request
-	//I assume $inputs will be of the form (appointment_id)
-	//updates appointment approved status to TRUE
+	
+	/**
+	 * Doctor approves an appointment request
+	 * 
+	 * @param $inputs
+	 *   Is of the form: array(appointment_id)
+	 * @return
+	 *   -1 in case of error in a query
+	 *   -5 if appointment_id does not exist
+	 *    0 if everything goes fine approved status is changed to TRUE
+	 * */
 	function approve($inputs){
+		
+		//test to see if the appointment_id exists
+		$sql = "SELECT *
+				FROM appointments A
+				WHERE A.appointment_id = ?"
+		$query = $this->db->query($sql, $inputs);
+		
+		if ($this->db->trans_status() === FALSE)
+			return -1;
+			
+		if ($query->num_rows() < 1)
+			return -5;
 		
 		//$data = array('approved' => TRUE);
 		//$this->db->update('Appointments', $data, array('appointment_id' => $inputs));
 		
 		$sql = "UPDATE appointments
-			SET approved = TRUE
-			WHERE appointment_id = ?";
+				SET approved = TRUE
+				WHERE appointment_id = ?";
 		$query = $this->db->query($sql, $inputs);
 		
+		if ($this->db->trans_status() === FALSE)
+			return -1;
 		
+		return 0;
 	}
 	
-	//patient OR doctor cancels appointment
-	//I assume $inputs will be of the form (appointment_id)
-	//deletes the apointment fromt the Appointments table
+
+	/**
+	 * Patient OR doctor cancels appointment
+	 * 
+	 * @param $inputs
+	 *   Is of the form: array(appointment_id)
+	 * @return
+	 *   -1 in case of error in a query
+	 *   -5 if appointment_id does not exist
+	 *    0 if everything goes fine and the appointment is deleted from the appointments table
+	 * */
 	function cancel($inputs){
+		
+		//test to see if the appointment_id exists
+		$sql = "SELECT *
+				FROM appointments A
+				WHERE A.appointment_id = ?"
+		$query = $this->db->query($sql, $inputs);
+		
+		if ($this->db->trans_status() === FALSE)
+			return -1;
+			
+		if ($query->num_rows() < 1)
+			return -5;
 		
 		//$this->db->delete('Appointments', array('appointment_id' => $inputs));
 		
 		$sql = "DELETE FROM appointments
-			WHERE appointment_id = ?";
+				WHERE appointment_id = ?";
 		$query = $this->db->query($sql, $inputs);
+		
+		if ($this->db->trans_status() === FALSE)
+			return -1;
+		
+		return 0;
 	}
+
 	
-	//patient reschedules appointment with the doctor
-	//I assume $inputs will be of the form (appointment_id, date_time (YY-MM-DD HH-MM-SS))
-	//updates date_time to new date and time
-	//NOTE: AS OF RIGHT NOW IT DOES NOT HANDLE ERROR CHECKING TO SEE IF THE date_time SPECIFIED CONFLICTS WITH ANOTHER APPOINTMENT
+	/**
+	 * Patient reschedules appointment with the doctor
+	 * 
+	 * @param $inputs
+	 *   Is of the form: array(appointment_id, date_time (YY-MM-DD HH-MM-SS))
+	 * @return
+	 *   -1 in case of error in a query
+	 *   -5 if appointment_id does not exist
+	 *    0 if everything goes fine and the appointment date_time is updated in the appointments table
+	 * @note
+	 * 	  AS OF RIGHT NOW IT DOES NOT HANDLE ERROR CHECKING TO SEE IF THE date_time SPECIFIED CONFLICTS WITH ANOTHER APPOINTMENT	
+	 * */
 	function reschedule($inputs){
+	
+		//test to see if the appointment_id exists
+		$sql = "SELECT *
+				FROM appointments A
+				WHERE A.appointment_id = ?"
+		$query = $this->db->query($sql, array($inputs[0]));
+		
+		if ($this->db->trans_status() === FALSE)
+			return -1;
+			
+		if ($query->num_rows() < 1)
+			return -5;
 	
 		//$data = array('date_time' => $inputs[1]);
 		//this->db-update('Appointments', $data, array('appointment_id' => $inputs));
 		
 		$sql = "UPDATE appointments
-			SET date_time = ?
-			WHERE appointment_id = ?";
+				SET date_time = ?
+				WHERE appointment_id = ?";
 		$query = $this->db->query($sql, array($inputs[1], $inputs[0]));
+		
+		if ($this->db->trans_status() === FALSE)
+			return -1;
+		
+		return 0;
 	}
-
-
 }
 ?>
