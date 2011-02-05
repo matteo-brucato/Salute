05858f455d43f1ae14840ecd4bcfccfb63843a45
musa <musa@musa-VirtualBox.(none)>
2011-02-05 18:35:26 -0500

    updated get_patient_records

--- system/application/models/medical_records_model.php
+++ system/application/models/medical_records_model.php
@@ -306,11 +306,15 @@ class Medical_records_model extends Model {
 	 *   -1 if error in query
 	 *    Array with all of the medical records
 	 * */
-	 function get_patient_records($inpts){
-		 
-		$sql = "SELECT *
-			FROM medical_record M, permission P
-			WHERE M.patient_id = ? AND M.medical_rec_id = P.medical_rec_id AND P.account_id = ?";
+	 function get_patient_records($inpts){			 
+		$sql = "(SELECT M.*, P.first_name AS pat_first_name, P.last_name AS pat_last_name, A.*, H.first_name, H.last_name
+			FROM patient_account P, accounts A, hcp_account H, medical_record M, permission PR
+			WHERE M.patient_id = ? AND M.patient_id = P.account_id AND M.medical_rec_id = PR.medical_rec_id AND P.account_id = ? AND P.account_id = A.account_id AND A.ccount_id = H.account_id)
+			UNION
+			(SELECT M.*, P.first_name AS pat_first_name, P.last_name AS pat_last_name, A.*, P2.first_name, P2.last_name
+			FROM patient_account P, accounts A, hcp_account H, medical_record M, permission PR
+			WHERE M.patient_id = ? AND M.patient_id = P.account_id AND M.medical_rec_id = PR.medical_rec_id AND P.account_id = ? AND P.account_id = A.account_id AND A.ccount_id = P2.account_id)";
+			
 		$query = $this->db->query($inputs);
 		
 		if ($this->db->trans_status() === FALSE)
