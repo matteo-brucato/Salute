a85f8d5ed2347cc3da4a10190281710cb90c0f45
Nada <nadahashem@gmail.com>
2011-02-06 14:39:03 -0800

    send message tested and complete :)

--- system/application/views/mainpane/send_message.php
+++ system/application/views/mainpane/send_message.php
@@ -1,14 +1,13 @@
 <form method="post" action="/messages/send/<?php echo $acc['account_id'];?>" id="send-message">
 	<fieldset id="send-message-fieldset">
 		<legend>Compose Email</legend>
-		<table>
-			<tr>
-				<td><body><b>To: </b><?php echo $info['first_name'].' '.$info['middle_name'].' '.$info['last_name'].' &lt;'.$acc['email'].'&gt;';?></body></td>
-			</tr>
-			<tr>
-				<td colspan="2"><textarea name="body" cols="70" rows="10" wrap="hard">Write your message here...</textarea></td>
-			</tr>
-		</table>
+		
+		<b>To: </b><?php echo $info['first_name'].' '.$info['middle_name'].' '.$info['last_name'].' &lt;'.$acc['email'].'&gt;';?>
+			
+		<br/><label for="subject">Subject: </label>
+		<input type="text" name="subject" class="input-field"/><br />
+		<textarea name="body" cols="70" rows="10" wrap="hard">Write your message here...</textarea>
+		
 		<p>
 			<input type="submit" name="submit" value="Send" class="submit-button" />
 		</p>
