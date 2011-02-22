2ae14770e93a8a08dde2cad3cfb53f1de5b39f2d
MR <musa@musa-laptop.(none)>
2011-02-21 22:32:36 -0800

    created view for referals

---
+++ system/application/views/mainpane/lists/referals.php
@@ -0,0 +1,67 @@
+<?php
+$this->load->helper('actions');
+$this->load->helper('table_result');
+$this->load->library('auth');
+
+/**
+ * @param
+ *   $list_name		The name of the list
+ *   $list			The result array from the database
+ *   $status		Must be one of:
+ * 					
+ * 					
+ * 
+ * @note For now we are assuming that a controller asks a list where
+ * ALL the tuples are of the same status.
+ * */
+
+echo '<h2>'.$list_name.'</h2>';
+
+// Id of the table
+$table['table-name'] = 'myreferals-table';
+
+// Names of the headers in the table
+$table['th'] = array( 'First Name', 'Last Name', 'First Name', 'Last Name', 'Specialization', 'Date & Time', 'Status', 'Actions');
+
+// Classes for columns (order matters)
+$table['th_class'] = array( '', '', '', '', '', '', '', '', '');
+$table['td_class'] = array( '', '', '', '', '', '', '', '', '');
+
+// All the results from the database
+$table['tuples'] = $list;
+
+// Attributes to display
+if ($this->auth->get_type() === 'patient')
+	$table['attr'] = array( 'ref_FN', 'ref_LN','is_ref_FN', 'is_ref_LN', 'specialization', 'date_time', 'status', 'actions');
+else
+	$table['attr'] = array( 'pat_FN', 'pat_LN','is_ref_FN', 'is_ref_LN', 'specialization', 'date_time', 'status', 'actions');
+
+for ($i = 0; $i < count($table['tuples']); $i++) {
+	
+	if( $table['tuples'][$i]['status'] === FALSE ){
+		$table['tuples'][$i]['status'] = 'pending';
+	}
+	else{
+		$table['tuples'][$i]['status'] = 'Request Sent';
+	}
+
+	if ($this->auth->get_type() === 'patient') {
+		if ($table['tuples'][$i]['status'] === 'pending')
+			$actions = array('accept-ref', 'delete-ref');
+		else
+			$actions = array();
+	} 
+	else {
+		if ($table['tuples'][$i]['status'] === 'Request Sent')
+			$actions = array();
+		else
+			$actions = array('delete-ref');
+			
+	}
+	
+	$table['tuples'][$i]['actions'] = '<ul>';
+	$table['tuples'][$i]['actions'] .= get_action_strings($actions, $list[$i]);
+	$table['tuples'][$i]['actions'] .= '<ul>';
+}
+view_table($table);
+?>
