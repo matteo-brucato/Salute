4260e106561d9d4b6600fdf5356454fb9c93cc7b
MR <musa@musa-laptop.(none)>
2011-03-03 01:12:14 -0800

    created new test data

--- database/data/test.sh
+++ database/data/test.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 #generate information for hcp account
 
+counter=2;
+for ((i = 1; i <= 50; i++))
+do
+	for(( j = 1 ; j < 5 && i <= 50 && counter <=50; j++))
+	do
+		if [ `expr $counter % 2` -eq 0 ]; then
+			echo $i";"$counter";true;2011-03-01" >>  connections.txt
+		else
+			echo $i";"$counter";false;2011-03-01" >>  connections.txt
+		fi
+		((counter++))
+	done
+done
+
